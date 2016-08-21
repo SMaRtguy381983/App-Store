a39ab01b709fb15740a8d8fa0ee05a76ce39e6f4
Shaun M. Rogers <SMRogers@fullsail.edu>
2016-08-21 18:25:57 -0400

    Required loot-ytilitu in app.js

--- src/routes/api/user.js
+++ src/routes/api/user.js
@@ -1,6 +1,6 @@
 const user = require('../../models/user');
 // Define our log module
-// const utilityTool = require('loot-ytilitu');
+const utilityTool = require('loot-ytilitu');
 
 module.exports = (express) => {
   const router = express.Router();
