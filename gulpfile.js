fe6206f090bf0862c3f99986031ca6ba228a45ee
Shaun M. Rogers <SMRogers@fullsail.edu>
2016-08-25 13:06:05 -0400

    Testing to see if gulp properly functioning, if so it should add all files and commit locally

--- gulpfile.js
+++ gulpfile.js
@@ -3,21 +3,37 @@ const git = require('gulp-git');
 const argv = require('yargs').argv;
 const utilityTool = require('loot-ytilitu');
 
+// Git Release Tasks
+
 gulp.task('addAll', () => {
+  utilityTool.debug('Hey! You are adding all!', {}, 1);
+
   return gulp.src('./')
     .pipe(git.add({ args: '-A' }));
 });
 
-gulp.task('commit', () => {
-  // place code for your default task here
-  return gulp.src('./gulpfile.js')
-    .pipe(git.commit('auto commit message'));
+gulp.task('commit', ['addAll'], () => {
+  if (argv.m) {
+    utilityTool.debug('Yo! You are committing', {}, 1);
+
+    return gulp.src('./')
+      .pipe(git.commit(argv.m));
+  }
+
+  utilityTool.debug('--m was not given to the gulp cmd, but it must be the commit message', {}, 1);
+
+  return false;
 });
 
-gulp.task('release', () => {
-
+gulp.task('push', ['commit'], () => {
+  // utilityTool.debug('Push it real good!', {}, 1);
 });
 
+gulp.task('release', ['push'], () => {
+  // utilityTool.debug('You are doing everything!', {}, 1);
+});
+
+// Default task
 gulp.task('default', [], () => {
-  utilityTool.debug('Just a default Gulp task!' {}, 1);
+  utilityTool.debug('Just a default Gulp task!', {}, 1);
 });
